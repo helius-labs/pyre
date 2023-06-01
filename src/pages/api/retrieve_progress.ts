a02fd6470d59275e98dcd6a1e836a56a2a4de333
Tidelaw <116292851+Tidelaw@users.noreply.github.com>
2023-06-01 10:41:35 +0700

    saving user progress

--- src/pages/api/retrieve_progress.ts
+++ src/pages/api/retrieve_progress.ts
@@ -1,26 +1,22 @@
 import { supabase } from '../../../supabaseClient';
 
 export default async function handler(req: any, res: any) {
-
     try {
         if (req.method === "POST") {
 
-            let user_data = {
-                user: req.body.user,
-                questions_remaining: req.body.questions_remaining,
-                progress: req.body.progress
-            }
-
             const { data, error } = await supabase
                 .from('users')
-                .upsert(user_data)
+                .select()
+                .eq('user', req.body.user)
 
             res.status(200).json(data)
 
         };
 
     }
-
     catch (err) { console.log(err) }
 }
+
+
+
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,6 +30,88 @@ export default function Home() {
   const [selectedComponent, setSelectedComponent] = useState('Landing')
   const [question, setQuestion] = useState()
   const [progress, setProgress] = useState(0)
+  const originalQuestions = [
+    {
+      name: "Find the number of NFTs held by a wallet",
+      description: "You are provided a wallet address. Make use of Helius's service to determine the number of NFTs held by the provided wallet.",
+      difficulty: 1,
+      api: 'nfts_held',
+      solved: false,
+      type: 'wallet',
+      example_answer: "25",
+      tags: ["DAS", "RPC"]
+    },
+    {
+      name: "Extract the image URL of a NFT",
+      description: "You are provided the mint address of a token. Make use of Helius's services to find the image of an NFT, you'll need to extract the URL pointing to the image from the data.",
+      difficulty: 1,
+      api: 'image_nft',
+      solved: false,
+      type: 'nft',
+      example_answer: "https://madlads.s3.us-west-2.amazonaws.com/images/8420.png",
+      tags: ["NFT API", "DAS", "RPC"]
+    },
+    {
+      name: "Identify the NFT's holder",
+      description: "You are provided the mint address of a token. Make use of Helius's services to identify the holder of the provided NFT token address.",
+      difficulty: 1,
+      api: 'nft_holder',
+      solved: false,
+      type: 'nft',
+      example_answer: "T1d3crwf5cYLcVU5ojNRgJbJUXJta2uBgbtev2xWLAW",
+      tags: ["DAS", "RPC"]
+    },
+    {
+      name: "Identify the epoch time of a transaction",
+      description: "You are provided the signature of a transaction. Make use of Helius's services to identify the time at which the provided transaction took place.",
+      difficulty: 1,
+      api: 'epoch_tx',
+      solved: false,
+      type: 'tx',
+      example_answer: "1633112174",
+      tags: ["ENHANCED API"]
+    },
+    {
+      name: "Find the first transaction of a wallet",
+      description: "You are provided a wallet address. Make use of Helius's services in order to retrieve the first transaction signature of the provided wallet.",
+      difficulty: 2,
+      api: "first_tx",
+      solved: false,
+      type: "wallet",
+      example_answer: "42JQVGf7V6LzAMizHEMk8tJ1HPozrBmB4dCxNgU14CSx8sXLxWau3JsS2NcM8vnDYK2XSXXhnNSVN8zfnBqiqGDd",
+      tags: ["RPC"]
+    },
+    {
+      name: "Find the SOL balance of a wallet",
+      description: "You are provided a wallet address. Make use of Helius's services in order to retrieve the wallet's native balance, otherwise known as SOL (data should be inputted rounded to 2 decimal places).",
+      difficulty: 1,
+      api: "sol_held",
+      solved: false,
+      type: "wallet",
+      example_answer: "25.01",
+      tags: ["ENHANCED API"]
+    },
+    {
+      name: "Find the number of times an NFT has been sold.",
+      description: "You are provided a token address. Make use of Helius's services in order to find the number of times it has been sold since being minted.",
+      difficulty: 1,
+      api: "times_sold",
+      solved: false,
+      type: "nft",
+      example_answer: "5",
+      tags: ["NFT API"]
+    },
+    {
+      name: "Find the supply of a collection.",
+      description: "You are provided a token address. Make use of Helius's services in order to retrieve the token's collection address, and use that in order to figure out the supply of the collection (this does not include burned NFTs).",
+      difficulty: 2,
+      api: "nft_supply",
+      solved: false,
+      type: "nft",
+      example_answer: "9999",
+      tags: ["ENHANCED API", "RPC", "DAS"]
+    },
+  ]
   const [questions, setQuestions] = useState<Questions[]>([
     {
       name: "Find the number of NFTs held by a wallet",
@@ -113,7 +195,8 @@ export default function Home() {
     },
   ])
   const { publicKey } = useWallet();
-  const [wallet, setWallet] = useState('')
+  const [wallet, setWallet] = useState('');
+  const [currentWallet, setCurrentWallet] = useState('')
 
   useEffect(() => {
 
@@ -124,7 +207,7 @@ export default function Home() {
           questions_remaining: questions,
           progress: progress,
         });
-      console.log(data)
+
     }
 
     if (publicKey) {
@@ -133,15 +216,40 @@ export default function Home() {
 
   }, [progress])
 
+
+  useEffect(() => {
+
+
+    async function retrieveProgress() {
+
+      const { data } = await axios.post("/api/retrieve_progress",
+        {
+          user: publicKey?.toBase58()
+        })
+      setProgress(data[0].progress)
+      setQuestions(data[0].questions_remaining)
+
+    }
+
+    if (publicKey) {
+      retrieveProgress()
+    }
+    else {
+      setProgress(0)
+      setQuestions(originalQuestions)
+    }
+
+  }, [publicKey])
+
   return (
     <main className={`flex w-full h-screen flex-col items-center justify-between font-sans bg-neutral-950 text-zinc-200`}>
 
 
       {selectedComponent === "Landing" ? (
         <>
-            <WalletMultiButtonDynamic className='relative bg-zinc-900 hover:bg-zinc-900 hover:opacity-100 duration-200' />
+          <WalletMultiButtonDynamic className='relative bg-zinc-900 hover:bg-zinc-900 hover:opacity-100 duration-200' />
 
-            <Landing setSelectedComponent={setSelectedComponent} />
+          <Landing setSelectedComponent={setSelectedComponent} />
         </>
       ) : (
         selectedComponent === "Menu" ? (
